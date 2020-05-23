975eff643250a570bda952752f1684e8af03eb85
songseungeun <grayandjj@gmail.com>
2020-05-23 17:30:27 +0900

    add main contents

--- src/Components/Pages/Main.js
+++ src/Components/Pages/Main.js
@@ -4,14 +4,14 @@ import './Main.css';
 import Map from '../Templates/Maps/Map';
 import Test from '../Templates/Test/Test'; // 테스트용 컴포넌트(삭제예정)
 // import { MapContext } from '../../ContextAPI/MapContext';
-import MaskList from '../Templates/Maps/MaskList';
+import MainContents from './MainContents';
 
 function Main() {
   // const { state } = useContext(MapContext);
   return (
     <main className="Main">
       <Test />
-      <MaskList />
+      <MainContents />
       <Map />
     </main>
   );
