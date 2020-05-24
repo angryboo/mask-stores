6172e2f6dc41c7f5bff4e30b7388764db1cfef56
angryboo <buyongsong@gmail.com>
2020-05-24 14:57:03 +0900

    fix: remove console

--- src/Components/Templates/Maps/MapEvent.js
+++ src/Components/Templates/Maps/MapEvent.js
@@ -6,7 +6,6 @@ function MapEvent() {
   const { state, getStores } = useContext(MapContext);
 
   useEffect(() => {
-    console.log(111);
     getStores(state.latitude, state.longitude, state.radius);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [state.latitude, state.longitude, state.radius]);
